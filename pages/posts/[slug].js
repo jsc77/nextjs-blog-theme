703b37b56e6d9b28835170bb946f6e45fe3b8934
jsc77 <65497652+jsc77@users.noreply.github.com>
2022-09-13 20:23:32 +0900

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -12,7 +12,6 @@ import Link from 'next/link';
 import ArrowIcon from '../../components/ArrowIcon';
 import CustomLink from '../../components/CustomLink';
 import Footer from '../../components/Footer';
-import Header from '../../components/Header';
 import Layout, { GradientBackground } from '../../components/Layout';
 import SEO from '../../components/SEO';
 
