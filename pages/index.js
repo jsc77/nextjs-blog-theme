7bce8e6ef687ca5c8d73580247ac1875956103ce
jsc77 <65497652+jsc77@users.noreply.github.com>
2022-09-13 20:09:07 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,9 @@ export default function Index({ posts, globalData }) {
     <Layout>
       <SEO title={globalData.name} description={globalData.blogTitle} />
       <main className="w-full">
-        <h1 className="text-3xl lg:text-5xl text-center mb-12">포트폴리오</h1>
+        <h1 className="text-3xl lg:text-5xl text-center mb-12 invisible">
+          {globalData.blogTitle}
+        </h1>
         <ul className="w-full">
           {posts.map((post) => (
             <li
